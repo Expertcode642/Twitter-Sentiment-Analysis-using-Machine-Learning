39e19dace16d01391680f9509c296225b7e8a6d4
Expertcode642 <144777591+Expertcode642@users.noreply.github.com>
2023-09-12 13:46:11 +0500

    Add files via upload

---
+++ Sentiment score.py
@@ -0,0 +1,10 @@
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["tweet"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["tweet"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["tweet"]]
+data = data[["tweet", "Positive", 
+             "Negative", "Neutral"]]
+print(data.head())
